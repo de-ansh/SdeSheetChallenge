87142dc84a676a474dbcb83b6b58eb2db7ddc5b0
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-06 22:50:44 +0530

    added another appraoch

--- Arrays/matrixsearch.cpp
+++ Arrays/matrixsearch.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 #include <vector>
-
+/*
 bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
@@ -10,4 +10,22 @@ bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target
         }
     }
      return false;
+}
+*/
+
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    int low=0;
+    if(!mat.size()) return false;
+    int high=(m*n)-1;
+    while(low<=high){
+        int mid = (low + (high - low) / 2);
+        if(mat[mid/n][mid%n]==target)
+            return true;
+        if(mat[mid/n][mid%n]<target)
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+    return false;
 }
