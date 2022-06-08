6b16b5e6d0e5b97a242789ae1d9f039561ec76a9
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-09 00:54:58 +0530

    solved

--- Arrays/longestsequence.cpp
+++ Arrays/longestsequence.cpp
@@ -1,1 +1,24 @@
-longestsequence
+#include <bits/stdc++.h> 
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+    if(arr.size() == 0 ){
+            return 0;
+        }
+        
+        sort(arr.begin(),arr.end());
+        
+        int ans = 1;
+        int prev = arr[0];
+        int cur = 1;
+        
+        for(int i = 1;i < arr.size();i++){
+            if(arr[i] == prev+1){
+                cur++;
+            }
+            else if(arr[i] != prev){
+                cur = 1;
+            }
+            prev = arr[i];
+            ans = max(ans, cur);
+        }
+        return ans;
+}
