051629cdad7a0ffbae2e3486d75d2955e084b281
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-11 00:26:25 +0530

    solved

--- linkedlist/palindromelist.cpp
+++ linkedlist/palindromelist.cpp
@@ -52,3 +52,15 @@ bool isPalindrome(LinkedListNode<int> *head) {
     return true;
 
 }
+
+bool isPalindrome(LinkedListNode<int> *head) {
+    vector<int> arr;
+    while(head != NULL) {
+        arr.push_back(head->data);
+        head = head->next;
+    }
+    for(int i=0;i<arr.size()/2;i++) 
+        if(arr[i] != arr[arr.size()-i-1]) return false;
+    return true;
+
+}
