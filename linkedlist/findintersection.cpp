a074368f7c8c60579bccb1480ec402849bade9a7
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-10 23:40:52 +0530

    solved

--- linkedlist/findintersection.cpp
+++ linkedlist/findintersection.cpp
@@ -75,3 +75,37 @@ int main() {
     cout<<"The intersection point is "<<answerNode->num<<endl;
     return 0;
 }
+/*
+#include <bits/stdc++.h> 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+        };
+
+*****************************************************************
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+    unordered_set<Node*> st;
+    
+    while(firstHead!=NULL){
+        st.insert(firstHead);
+        firstHead=firstHead->next;
+    }
+       while(secondHead!=NULL){
+             if(st.find(secondHead) != st.end()) return secondHead->data;
+           secondHead=secondHead->next;
+        }
+    return -1;
+}*/
