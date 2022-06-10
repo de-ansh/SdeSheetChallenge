7673994dc1e0967e308a44968118755ecd66fb35
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-10 23:51:27 +0530

    solved

--- linkedlist/detectcycle.cpp
+++ linkedlist/detectcycle.cpp
@@ -10,3 +10,15 @@ bool detectCycle(Node *head)
     }
     return false;
 }
+
+
+bool detectCycle(Node *head)
+{
+    unordered_set<Node*> hashTable;
+    while(head != NULL) {
+        if(hashTable.find(head) != hashTable.end()) return true;
+        hashTable.insert(head);
+        head = head->next;
+    }
+    return false;
+}
