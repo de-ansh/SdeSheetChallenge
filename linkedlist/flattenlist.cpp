8438cdad661f518cac798c1255eae983629c8efd
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-13 23:08:32 +0530

    solved

--- linkedlist/flattenlist.cpp
+++ linkedlist/flattenlist.cpp
@@ -58,3 +58,65 @@ Node *flattenLinkedList(Node *root)
         // it will be in turn merged with its left 
         return root; 
 }
+
+
+/*#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* child;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+        this->child = NULL;
+    }
+};
+
+*****************************************************************
+
+Node* mergeTwoLists(Node* a, Node* b) {
+    
+    Node *temp = new Node(0);
+    Node *res = temp; 
+    
+    
+    while(a != NULL && b != NULL) {
+        if(a->data < b->data) {
+            temp->child = a; 
+            temp = temp->child; 
+            a = a->child; 
+        }
+        else {
+            temp->child = b;
+            temp = temp->child; 
+            b = b->child; 
+        }
+    }
+    
+    if(a) temp->child = a; 
+    else temp->child = b; 
+    
+    return res -> child; 
+    
+}
+Node *flattenLinkedList(Node *root)
+{
+   
+        if (root == NULL || root->next == NULL) 
+            return root; 
+  
+        // recur for list on right 
+        Node *right=flattenLinkedList(root->next);
+
+root->next=NULL;
+
+  Node *ans=mergeTwoLists(root,right);
+    return ans;
+}
+*/
