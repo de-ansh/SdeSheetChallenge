74ad5ff61e3b6a66c8adfc711492b0fee9b5e8e6
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-16 23:02:27 +0530

    solved

--- Recursion/kthpermutation.cpp
+++ Recursion/kthpermutation.cpp
@@ -23,3 +23,60 @@ string kthPermutation(int n, int k) {
     auto it = res.begin() + (k - 1);
     return *it;
 }
+
+
+void solve(string s,int k,string &ans,int fact)
+
+{
+
+   if(k==0)
+
+   {
+
+       ans.append(s);
+
+       return;
+
+   }
+
+   
+
+   ans.push_back(s[k/fact]);
+
+   s.erase(s.begin()+k/fact);
+
+   solve(s,k%fact,ans,fact/s.length());
+
+}
+
+ 
+
+string kthPermutation(int n, int k) {
+
+   // Write your code here.
+
+   string s="";
+
+   int fact=1;
+
+   for(int i=1;i<=n;i++)
+
+   {
+
+       s.append(to_string(i));
+
+       fact*=i;
+
+   }
+
+   fact/=n;
+
+   string ans="";
+
+   solve(s,k-1,ans,fact);
+
+   return ans;
+
+}
+
+ 
