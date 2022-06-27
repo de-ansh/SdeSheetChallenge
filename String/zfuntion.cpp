3ef51b8ed030c3708300a26227be2c8551ee20f9
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-28 00:30:36 +0530

    solved

---
+++ String/zfuntion.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int zAlgorithm(string s, string p, int n, int m)
+{
+	int count=0;
+	for(int i=0;i<n-m+1;i++){
+		string ans=s.substr(i,m);
+		if(ans==p){
+			count++;
+		}
+	}
+	return count;
+}
