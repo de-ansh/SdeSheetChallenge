7b4445fa3ea01ec7a147a195053e7b532769bdc7
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-27 23:36:48 +0530

    solved

--- String/longestcommonstring.cpp
+++ String/longestcommonstring.cpp
@@ -1,1 +1,24 @@
-longestcommonstring.cpp
+#include<bits/stdc++.h>
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    // Write your code here
+    string ans;
+    sort(arr.begin(),arr.end());
+    
+    string a = arr[0];
+    string b = arr[n-1];
+    
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]==b[i])
+        {
+            ans += a[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    return ans;
+}
