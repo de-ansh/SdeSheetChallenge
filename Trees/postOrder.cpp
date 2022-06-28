ab546e824c1a85bfb9ced53af1037465401334a1
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-29 01:08:05 +0530

    solved on 29th june

--- Trees/postOrder.cpp
+++ Trees/postOrder.cpp
@@ -1,1 +1,27 @@
-postOrder.cpp
+#include <bits/stdc++.h>
+using namespace std;
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void postOrder(vector<int> & ans, TreeNode * root){
+    if(root==NULL)
+        return;
+    postOrder(ans,root->left);
+    postOrder(ans,root->right);
+        ans.push_back(root->data);
+}
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+    vector<int>ans;
+    postOrder(ans, root);
+    return ans;
+}
--- Trees/preOrder.cpp
+++ Trees/preOrder.cpp
@@ -1,1 +1,27 @@
-preOrder.cpp
+#include <bits/stdc++.h>
+using namespace std;
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void preOrder(vector<int> & ans, TreeNode *root){
+    if(root ==NULL)
+        return;
+    ans.push_back(root->data);
+    preOrder(ans,root->left);
+    preOrder(ans, root->right);
+}
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    preOrder(ans, root);
+    return ans;
+}
