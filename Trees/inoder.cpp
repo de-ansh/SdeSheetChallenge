de152d26e44bc1289c634299145b5d35814f53ff
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-06-29 13:43:18 +0530

    solved on 29th of june

--- Trees/inoder.cpp
+++ Trees/inoder.cpp
@@ -52,3 +52,35 @@ vector<int> getInOrderTraversal(TreeNode *root)
     return inOrder;
 }
 */
+
+
+/*
+
+vector<int> getPostOrderTraversal(TreeNode *root){
+    vector<int> postOrder;
+    if(root==NULL)
+        return postOrder;
+    stack<TreeNode*> st;
+    while(root!=NULL || !st.empty()){
+        if(root!=NULL){
+            st.push(root);
+            root=root->left;
+            }
+        else{
+            TreeNode * temp= st.top()->right;
+            if(temp==NULL){
+                temp=st.top();
+                st.pop();
+                postOrder.push_back(temp->data);
+                while(!st.empty() && temp==st.top()->right){
+                    temp=st.top();
+                    st.pop();
+                    postOrder.push_back(temp->data);
+                }
+            }
+            else root=temp;
+        }
+    }
+    return postOrder;
+}
+*/
--- Trees/postOrder.cpp
+++ Trees/postOrder.cpp
@@ -25,3 +25,32 @@ vector<int> getPostOrderTraversal(TreeNode *root)
     postOrder(ans, root);
     return ans;
 }
+
+/*
+
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+    vector<int>postOrder;
+    if(root==NULL)
+        return postOrder;
+    stack<TreeNode *> st1,st2;
+    st1.push(root);
+    while(!st1.empty()){
+        root=st1.top();
+        st1.pop();
+        st2.push(root);
+        if(root->left!=NULL){
+            st1.push(root->left);
+        }
+        if(root->right!=NULL)
+            st1.push(root->right);
+    }
+    while(!st2.empty())
+    {
+        postOrder.push_back(st2.top()->data);
+        st2.pop();
+    }
+    return postOrder;
+}
+
+*/
