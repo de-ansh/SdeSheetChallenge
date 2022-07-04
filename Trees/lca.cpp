4a8fe44601f7d76b29e4fe984df9ada0a2788941
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-04 22:41:57 +0530

    solved! on 4th july!

--- Trees/lca.cpp
+++ Trees/lca.cpp
@@ -55,3 +55,25 @@ TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q
     else
         return root;
 }*/
+
+
+//CodeStudio 2nd type of LCA
+/*int lowestCommonAncestor(TreeNode<int> *root, int x, int y)
+{
+	if(root == NULL){
+            return -1;
+        }
+        if(root->data == x || root->data == y){
+            return root->data;
+        }
+        
+        int left = lowestCommonAncestor(root->left, x, y);
+        int  right = lowestCommonAncestor(root->right, x ,y);
+        
+        if(left == -1)
+            return right;
+        else if (right == -1)
+            return left;
+        else
+            return root->data;
+}*/
