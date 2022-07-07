37b67785e0686197f13674cc306fbd27e47bba71
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-07 21:50:12 +0530

    solved on 7th of july

--- Trees/counttreeNodes.cpp
+++ Trees/counttreeNodes.cpp
@@ -16,3 +16,31 @@ public:
 
 
 //Since No. of nodes = 2^h-1;
+class Solution {
+public:
+    int leftheight(TreeNode *node){
+        int height=0;
+        while(node){
+            height++;
+            node=node->left;
+        }
+        return height;
+    }
+    int rightheight(TreeNode *node){
+        int height=0;
+        while(node){
+            height++;
+            node=node->right;
+            }
+        return height;
+    }
+    int countNodes(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int lh=leftheight(root);
+        int rh=rightheight(root);
+        if(lh==rh)
+            return (1<<lh)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
