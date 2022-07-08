1a00809d8a48a9e01360729c2710b3aa2e50cb9d
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-09 00:14:46 +0530

    solved on 8th of july

--- Trees/mosrrishTraversal.cpp
+++ Trees/mosrrishTraversal.cpp
@@ -1,1 +1,22 @@
-vector<int> Morris(TreeNode * )
+vector<int> getinotdrt(TreeNode * root ){
+	vector<int> inorder;
+	TreeNode * cur=root;
+	while(cur!= NULL){
+		if(cur->left==NULL){
+			inorder.push_back(cur->val);
+			cur=cur->right;
+		}
+		else{
+			TreeNode *prev= cur->left;
+			while(prev->right && prev->right!= cur){
+				prev=prev->right;
+			}
+			if(prev->right==NULL){
+				prev->right=curr;
+				cur=cur->left;
+			}
+
+		}
+
+	}
+}
