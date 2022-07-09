90fa50b4b8949a852fce465a0b06d6c9fd732ebf
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-09 23:24:55 +0530

    solved on 9th of july

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -1,10 +1,11 @@
 /*
 In binary Search tree: Its a node based tree which means
 
- Left<Root<Right
+ Logic: Left<Root<Right
 
       10
    9	  13
+ 1. Entire left subtree is a bst
+ 2. entire right subtree is a bst
 
-   
 */
