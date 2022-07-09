4282416c9f657581558f78a452909a4dcf43cbb7
Ashish Mishra <72343696+de-ansh@users.noreply.github.com>
2022-07-09 23:39:08 +0530

    solved on 9th of july

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -7,5 +7,17 @@ In binary Search tree: Its a node based tree which means
    9	  13
  1. Entire left subtree is a bst
  2. entire right subtree is a bst
+Height of Bst is log n generally
+Traversal is Log n=> Binary search (Hint)
+
 
 */
+
+//Search
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    while(root!=NULL && root->data!=x ){
+        root=x<root->data ?root->left: root->right;
+    }
+    return root;
+}
